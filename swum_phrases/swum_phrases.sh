3b739beb040d754aa8b4a759429f1fe2d5623edd
BrianPopeck <bpopeck4@gmail.com>
2020-07-03 15:37:25 -0400

    added script to generate antlr lexer and parser on the fly from grammar files

---
+++ swum_phrases/swum_phrases.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 'creating antlr files...'
+cd antlr
+java -Xmx500M -cp "/usr/local/lib/antlr-4.8-complete.jar:$CLASSPATH" org.antlr.v4.Tool -Dlanguage=Python3 SwumLexer.g4 SwumParser.g4 -visitor
+cd ..
+echo 'done'
+python3 SwumPhrases.py
+echo 'deleting antlr files...'
+cd antlr
+rm -r __pycache__ SwumLexer.interp SwumLexer.py SwumLexer.tokens SwumParser.interp SwumParser.py SwumParser.tokens SwumParserListener.py SwumParserVisitor.py
+echo 'done'
