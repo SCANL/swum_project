32f3c13932cbe415502120abe022efd19fb2e476
Aditya Bhargava <56522465+Bhargavaa1@users.noreply.github.com>
2020-07-08 12:24:27 -0400

    Update XML_Output_SAX_Parser.py

--- swum_words/XML_Output_SAX_Parser.py
+++ swum_words/XML_Output_SAX_Parser.py
@@ -13,170 +13,127 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 
 class JavaHandler(xml.sax.ContentHandler):
     def __init__(self):
+        self.interface = ""
+        self.variable = ""
+        self.function = ""
+        self.constructor = ""
+        self.type = ""
+        self.className = ""
         self.currentTag = ""
         self.currentContent = ""
         self.dictTag = {"decl": 0, "decl_stmt": 0, "type": 0, "name": 0, "class": 0, "function": 0, "function_decl": 0,
                         "expr": 0, "expr_stmt": 0, "parameter": 0, "parameter_list": 0, "operator": 0, "specifier": 0, "constructor": 0, "interface": 0, "interface_decl": 0}
-        self.dictPOS = {}  # Stores the part of speech of every identifier
-        self.sentence = ""  # Used for Part of Speech (POS) tagging
         self.xml = etree.Element('swum_identifiers')
+        # Used for parameters in functions and constructors
+        self.parameterList = []
+        self.parameterType = []
+        self.parameterDict = {}
+        # Used for Part of Speech (POS) tagging
+        self.dictPOS = {}  
+        self.sentence = ""  
+    #def PartOfSpeechTag():
         
 
     def startElement(self, tag, attributes):  # Call when an element starts
         for x in self.dictTag:
             if tag == x:
                 self.dictTag[tag] += 1
-
-    def endElement(self, tag):  # Call when an elements ends
+    def XMLWriter(self,locationTagText,tree,currentContent, parameterType):
         self.sentence = ""
+        swumIdentifierTag = etree.SubElement(tree, 'swum_identifier')
+        locationTag = etree.SubElement(swumIdentifierTag, 'location')
+        locationTag.text = locationTagText
+        nameTag = etree.SubElement(swumIdentifierTag, 'name')
+        nameTag.text = currentContent
+        identifier = etree.SubElement(swumIdentifierTag, 'identifier')
+        tempList = ronin.split(currentContent)
+        for word in tempList:
+            self.sentence = self.sentence + word + " "
+            self.text = word_tokenize(self.sentence)
+            self.dictPOS[currentContent] = nltk.pos_tag(self.text)
+        for splitWord in self.dictPOS[currentContent]:
+            wordTag = etree.SubElement(identifier, 'word')
+            wordTag.text = splitWord[0]
+            partOfSpeechTag = etree.SubElement(wordTag, 'pos')
+            partOfSpeechTag.text = splitWord[1]
+        if parameterType and self.parameterList:
+            typeTag = etree.SubElement(swumIdentifierTag, 'type')
+            self.XMLWriter("type", typeTag, self.type, '')
+    def functionAndConstructorXMLWriter(self):
+        swumIdentifierTag = etree.SubElement(self.xml, 'swum_identifier')
+        locationTag = etree.SubElement(swumIdentifierTag, 'location')
+        if self.constructor:
+            name = self.constructor
+            locationTag.text = "constructor"
+        else:
+            name = self.function
+            locationTag.text = "function"
+        classTag = etree.SubElement(swumIdentifierTag, 'class')
+        classTag.text = self.className
+        nameTag = etree.SubElement(swumIdentifierTag, 'name')
+        nameTag.text = name
+        if self.type:
+            typeTag = etree.SubElement(swumIdentifierTag, 'type')
+            self.XMLWriter("type", typeTag, self.type, '')
+        if self.parameterList:
+            parameterTag = etree.SubElement(swumIdentifierTag, 'parameters')
+            for key in self.parameterDict:
+                self.XMLWriter("parameter", parameterTag, key, self.parameterDict[key])
+        identifierTag = etree.SubElement(swumIdentifierTag, 'identifier')
+        tempList = ronin.split(name)
+        self.sentence = ""
+        for word in tempList:
+            self.sentence = self.sentence + word + " "
+            self.text = word_tokenize(self.sentence)
+            self.dictPOS[name] = nltk.pos_tag(self.text)
+        for splitWord in self.dictPOS[name]:
+            wordTag = etree.SubElement(identifierTag, 'word')
+            wordTag.text = splitWord[0]
+            partOfSpeechTag = etree.SubElement(wordTag, 'pos')
+            partOfSpeechTag.text = splitWord[1]
+        # Resetting the function, constructor, and parameter attributes
+        self.function = ""
+        self.constructor = ""
+        self.parameterList = []
+        self.parameterType = []
+        self.type = ""
+        self.parameterDict.clear()
+    def endElement(self, tag):  # Call when an elements ends
+        if tag == "parameter_list":
+            for tempNum in range(0,len(self.parameterList)):
+                self.parameterDict[self.parameterList[tempNum]] = self.parameterType[tempNum]
+            self.functionAndConstructorXMLWriter()
         if tag == "name":
-            # Eliminates the expression statments since variable names are already declared
             if not (self.dictTag["expr"] or self.dictTag["expr_stmt"]):
-                # Differentiates between type and name
-                if not (self.dictTag["type"]):
+                if not (self.dictTag["type"]) or self.dictTag["parameter_list"]:
                     if self.dictTag["class"] and not (self.dictTag["function_decl"] or self.dictTag["function"] or self.dictTag["parameter"] or self.dictTag["parameter_list"] or self.dictTag["constructor"]):
-                        # Stores the class name and assigns the key a "class" value
                         print("Class Name: ", self.currentContent)
-                        #classTag = etree.SubElement(self.xml, 'class') # Defines class tag
-                        swumIdentifierTag = etree.SubElement( # Defines swum_identifier tag
-                            self.xml, 'swum_identifier')
-                        locationTag = etree.SubElement( # Defines location tag
-                            swumIdentifierTag, 'location')
-                        locationTag.text = 'class'
-                        identifier = etree.SubElement( # Defines identifier tag
-                            swumIdentifierTag, 'identifier')
-                        tempList = ronin.split(self.currentContent) # Splits the current identifer into the respective words 
-                        for word in tempList: # Part of Speech Tagger 
-                            self.sentence = self.sentence + word + " " # Part of Speech Tagger functions when a sentence is used as a parameter
-                            self.text = word_tokenize(self.sentence)
-                            self.dictPOS[self.currentContent] = nltk.pos_tag(
-                                self.text)
-                        for splitWord in self.dictPOS[self.currentContent]: # Asserting each word to a wordTag and part of speech to a POS tag
-                            wordTag = etree.SubElement(identifier, 'word')
-                            wordTag.text = splitWord[0]
-                            partOfSpeechTag = etree.SubElement(wordTag, 'pos')
-                            partOfSpeechTag.text = splitWord[1]
-                    elif self.dictTag["constructor"]:
-                        print("Constructor Name: ", self.currentContent)
-                        swumIdentifierTag = etree.SubElement(
-                            self.xml, 'swum_identifier')
-                        location = etree.SubElement(swumIdentifierTag, 'location')
-                        location.text = 'constructor'
-                        identifier = etree.SubElement(swumIdentifierTag, 'identifier')
-                        tempList = ronin.split(self.currentContent)
-                        for word in tempList:
-                            self.sentence = self.sentence + word + " "
-                            self.text = word_tokenize(self.sentence)
-                            self.dictPOS[self.currentContent] = nltk.pos_tag(
-                                self.text)
-                        for splitWord in self.dictPOS[self.currentContent]:
-                            wordTag = etree.SubElement(identifier, 'word')
-                            wordTag.text = splitWord[0]
-                            partOfSpeechTag = etree.SubElement(wordTag, 'pos')
-                            partOfSpeechTag.text = splitWord[1]
-                    elif self.dictTag["parameter"] or self.dictTag["parameter_list"] and not (self.dictTag["class"]):
-                        print("Parameter Name: ", self.currentContent)
-                        #parameterTag = etree.SubElement(self.xml, 'parameter')
-                        swumIdentifierTag = etree.SubElement(
-                            self.xml, 'swum_identifier')
-                        location = etree.SubElement(
-                            swumIdentifierTag, 'location')
-                        location.text = 'parameter'
-                        identifier = etree.SubElement(
-                            swumIdentifierTag, 'identifier')
-                        tempList = ronin.split(self.currentContent)
-                        for word in tempList:
-                            self.sentence = self.sentence + word + " "
-                            self.text = word_tokenize(self.sentence)
-                            self.dictPOS[self.currentContent] = nltk.pos_tag(
-                                self.text)
-                        for splitWord in self.dictPOS[self.currentContent]:
-                            wordTag = etree.SubElement(identifier, 'word')
-                            wordTag.text = splitWord[0]
-                            partOfSpeechTag = etree.SubElement(wordTag, 'pos')
-                            partOfSpeechTag.text = splitWord[1]
+                        self.className = self.currentContent
+                        self.XMLWriter("class", self.xml, self.currentContent, '')
+                    elif self.dictTag["constructor"] and not(self.dictTag["parameter"]):
+                        print("Constructor Name: ", self.currentContent)  
+                        self.constructor = self.currentContent            
+                    elif self.dictTag["parameter_list"]:
+                        if self.dictTag["name"] and self.dictTag["parameter"]and self.dictTag["type"]:
+                            print("Parameter Type: ", self.currentContent)
+                            self.parameterType.append(self.currentContent)
+                        elif self.dictTag["name"] and self.dictTag["parameter"]:
+                            print("Parameter Name: ", self.currentContent)
+                            self.parameterList.append(self.currentContent)
                     elif self.dictTag["function"] or self.dictTag["function_decl"] and not (self.dictTag["parameter"] or self.dictTag["parameter_list"]):
                         print("Function Name: ", self.currentContent)
-                        #functionTag = etree.SubElement(self.xml, 'function')
-                        swumIdentifierTag = etree.SubElement(
-                            self.xml, 'swum_identifier')
-                        location = etree.SubElement(
-                            swumIdentifierTag, 'location')
-                        location.text = 'function'
-                        identifier = etree.SubElement(
-                            swumIdentifierTag, 'identifier')
-                        tempList = ronin.split(self.currentContent)
-                        for word in tempList:
-                            self.sentence = self.sentence + word + " "
-                            self.text = word_tokenize(self.sentence)
-                            self.dictPOS[self.currentContent] = nltk.pos_tag(
-                                self.text)
-                        for splitWord in self.dictPOS[self.currentContent]:
-                            wordTag = etree.SubElement(identifier, 'word')
-                            wordTag.text = splitWord[0]
-                            partOfSpeechTag = etree.SubElement(wordTag, 'pos')
-                            partOfSpeechTag.text = splitWord[1]
+                        self.function = self.currentContent
                     elif self.dictTag["interface"] or self.dictTag["interface_decl"] and not(self.dictTag["function_decl"] or self.dictTag["function"] or self.dictTag["parameter"] or self.dictTag["parameter_list"]):
                         print("Interface Name: ", self.currentContent)
-                        swumIdentifierTag = etree.SubElement(
-                            self.xml, 'swum_identifier')
-                        location = etree.SubElement(
-                            swumIdentifierTag, 'location')
-                        location.text = 'interface'
-                        identifier = etree.SubElement(
-                            swumIdentifierTag, 'identifier')
-                        tempList = ronin.split(self.currentContent)
-                        for word in tempList:
-                            self.sentence = self.sentence + word + " "
-                            self.text = word_tokenize(self.sentence)
-                            self.dictPOS[self.currentContent] = nltk.pos_tag(
-                                self.text)
-                        for splitWord in self.dictPOS[self.currentContent]:
-                            wordTag = etree.SubElement(identifier, 'word')
-                            wordTag.text = splitWord[0]
-                            partOfSpeechTag = etree.SubElement(wordTag, 'pos')
-                            partOfSpeechTag.text = splitWord[1]
+                        self.interface = self.currentContent 
+                        self.XMLWriter("interface", self.xml, self.currentContent, '')
                     elif self.dictTag["decl_stmt"] or self.dictTag["decl"] and not (self.dictTag["parameter"] or self.dictTag["parameter_list"]):
                         print("Variable Name: ", self.currentContent)
-                        #variableTag = etree.SubElement(self.xml, 'variable')
-                        swumIdentifierTag = etree.SubElement(
-                            self.xml, 'swum_identifier')
-                        location = etree.SubElement(
-                            swumIdentifierTag, 'location')
-                        location.text = 'variable'
-                        identifier = etree.SubElement(
-                            swumIdentifierTag, 'identifier')
-                        tempList = ronin.split(self.currentContent)
-                        for word in tempList:
-                            self.sentence = self.sentence + word + " "
-                            self.text = word_tokenize(self.sentence)
-                            self.dictPOS[self.currentContent] = nltk.pos_tag(
-                                self.text)
-                        for splitWord in self.dictPOS[self.currentContent]:
-                            wordTag = etree.SubElement(identifier, 'word')
-                            wordTag.text = splitWord[0]
-                            partOfSpeechTag = etree.SubElement(wordTag, 'pos')
-                            partOfSpeechTag.text = splitWord[1]
-                elif(self.dictTag["type"] and not(self.currentContent == 'int' or self.currentContent == 'void' or self.currentContent == 'boolean' or self.currentContent == 'double' or self.currentContent == 'float' or self.currentContent == 'char' or self.currentContent == 'String' or '[]' in self.currentContent)): # Filters out the primitive data types and arrays. 
+                        self.variable = self.currentContent
+                        self.XMLWriter("decl", self.xml, self.currentContent, '')
+                elif(self.dictTag["type"]):
                     print("Type: ", self.currentContent)
-                    #typeTag = etree.SubElement(self.xml, 'type')
-                    swumIdentifierTag = etree.SubElement(
-                        self.xml, 'swum_identifier')
-                    location = etree.SubElement(swumIdentifierTag, 'location')
-                    location.text = 'type'
-                    identifier = etree.SubElement(
-                        swumIdentifierTag, 'identifier')
-                    tempList = ronin.split(self.currentContent)
-                    for word in tempList:
-                        self.sentence = self.sentence + word + " "
-                        self.text = word_tokenize(self.sentence)
-                        self.dictPOS[self.currentContent] = nltk.pos_tag(
-                            self.text)
-                    for splitWord in self.dictPOS[self.currentContent]:
-                        wordTag = etree.SubElement(identifier, 'word')
-                        wordTag.text = splitWord[0]
-                        partOfSpeechTag = etree.SubElement(wordTag, 'pos')
-                        partOfSpeechTag.text = splitWord[1]
+                    self.type = self.currentContent
             self.dictTag[tag] -= 1
         elif tag == "specifier":
             print("Specifier: ", self.currentContent)
@@ -185,14 +142,7 @@ class JavaHandler(xml.sax.ContentHandler):
             for x in self.dictTag:
                 if tag == x:
                     self.dictTag[tag] -= 1
-        if tag == "name":
-            tempList = ronin.split(self.currentContent)
-            for word in tempList:
-                self.sentence = self.sentence + word + " "
-                self.text = word_tokenize(self.sentence)
-                self.dictPOS[self.currentContent] = nltk.pos_tag(self.text)
         self.currentTag = ""  # Call when a character is read
-
     def characters(self, content):
         self.currentContent = ""
         self.currentContent += content
