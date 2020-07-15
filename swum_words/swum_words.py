bfb1c4a231998b5307cfed7869bd81bde9c6c4e4
Aditya Bhargava <56522465+Bhargavaa1@users.noreply.github.com>
2020-07-15 11:46:49 -0400

    Update swum_words.py

--- swum_words/swum_words.py
+++ swum_words/swum_words.py
@@ -2,7 +2,6 @@
 # Date: Summer 2020
 # Organization: RIT REU Cultivating New Generation Software
 # Function: Takes an XML file and analyzes it based on identifier name and function using a SAX parser. Prints the results in a XML format.
-import sys
 # Part of Speech tagging tools
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
@@ -15,8 +14,9 @@ from lxml import etree
 
 class JavaHandler(xml.sax.ContentHandler):
     def __init__(self):
-        self.dictTag = {"decl": 0, "decl_stmt": 0, "type": 0, "name": 0, "class": 0, "function": 0, "function_decl": 0,
-                        "expr": 0, "expr_stmt": 0, "parameter": 0, "parameter_list": 0, "operator": 0, "specifier": 0, "constructor": 0, "interface": 0, "interface_decl": 0}
+        self.dictTag = {"decl": 0, "decl_stmt": 0, "type": 0, "name": 0, "class": 0, "function": 0, "function_decl": 0, "index":0,
+                        "expr": 0, "expr_stmt": 0, "parameter": 0, "parameter_list": 0, "operator": 0, "specifier": 0, "constructor": 0, "interface": 0, "interface_decl": 0, "argument":0, "argument_list":0}
+        self.previousTag = ""
         self.xmlResult = etree.Element('swum_identifiers')
         # Used to define attributes
         self.interfaceName = ""
@@ -25,6 +25,7 @@ class JavaHandler(xml.sax.ContentHandler):
         self.constructorName = ""
         self.variableType = ""
         self.functionType = ""
+        self.parameterTyp = ""
         self.className = ""
         self.currentContent = ""
         # Used for parameters in functions and constructors
@@ -54,7 +55,7 @@ class JavaHandler(xml.sax.ContentHandler):
                 partOfSpeechTag = etree.SubElement(wordTag, 'pos')
                 partOfSpeechTag.text = splitWord[1]
     # XML writer for class names, parameters, variable names, and interface names
-    def XMLWriter(self,locationTagText,currentContent,parameterType,tree): 
+    def XMLWriter(self,locationTagText,currentContent,parameterType,tree):
         swumIdentifierTag = etree.SubElement(tree, 'swum_identifier')
         locationTag = etree.SubElement(swumIdentifierTag, 'location')
         locationTag.text = locationTagText
@@ -67,32 +68,32 @@ class JavaHandler(xml.sax.ContentHandler):
         self.PartOfSpeechTag(currentContent, identifierTag)
     # XML writer for functions and constructor names
     def functionConstructorDeclXMLWriter(self,definedName, role, definedType):
-        swumIdentifierTag = etree.SubElement(self.xmlResult, 'swum_identifier')
-        locationTag = etree.SubElement(swumIdentifierTag, 'location')
-        if role == "constructor" or role == "function":
-            classTag = etree.SubElement(swumIdentifierTag, 'class')
-            classTag.text = self.className
-        name = definedName
-        locationTag.text = role
-        nameTag = etree.SubElement(swumIdentifierTag, 'name')
-        nameTag.text = name
-        if definedType:
-            typeTag = etree.SubElement(swumIdentifierTag, 'type')
-            self.XMLWriter("type",definedType, '', typeTag)
-        if self.parameterList:
-            parameterTag = etree.SubElement(swumIdentifierTag, 'parameters')
-            for key in self.parameterDict:
-                self.XMLWriter("parameter",key,self.parameterDict[key], parameterTag)
-        identifierTag = etree.SubElement(swumIdentifierTag, 'identifier')
-        self.PartOfSpeechTag(name, identifierTag)
-        # Resetting the function, constructor, and parameter attributes
-        self.functionName = ""
-        self.constructorName = ""
-        self.functionType = ""
-        self.variableType =""
-        self.parameterType = []
-        self.parameterList = []
-        self.parameterDict.clear()
+            swumIdentifierTag = etree.SubElement(self.xmlResult, 'swum_identifier')
+            locationTag = etree.SubElement(swumIdentifierTag, 'location')
+            if role == "constructor" or role == "function":
+                classTag = etree.SubElement(swumIdentifierTag, 'class')
+                classTag.text = self.className
+            name = definedName
+            locationTag.text = role
+            nameTag = etree.SubElement(swumIdentifierTag, 'name')
+            nameTag.text = name
+            if definedType:
+                typeTag = etree.SubElement(swumIdentifierTag, 'type')
+                self.XMLWriter("type",definedType, '', typeTag)
+            if self.parameterList:
+                parameterTag = etree.SubElement(swumIdentifierTag, 'parameters')
+                for key in self.parameterDict:
+                    self.XMLWriter("parameter",key,self.parameterDict[key], parameterTag)
+            identifierTag = etree.SubElement(swumIdentifierTag, 'identifier')
+            self.PartOfSpeechTag(name, identifierTag)
+            # Resetting the function, constructor, and parameter attributes
+            self.functionName = ""
+            self.constructorName = ""
+            self.functionType = ""
+            self.variableType =""
+            self.parameterType = []
+            self.parameterList = []
+            self.parameterDict.clear()
     # Call when an elements ends
     def endElement(self, tag):
         # The function and constructor information is complete and therefore sent to their XML writer
@@ -100,46 +101,95 @@ class JavaHandler(xml.sax.ContentHandler):
             for tempNum in range(0,len(self.parameterList)):
                 self.parameterDict[self.parameterList[tempNum]] = self.parameterType[tempNum]
             if self.functionName:
-                self.functionConstructorDeclXMLWriter(self.functionName, "function", self.functionType)
-            else:
+                tempType = self.functionType
+                self.functionConstructorDeclXMLWriter(self.functionName, "function",tempType)
+            elif self.constructorName:
                 self.functionConstructorDeclXMLWriter(self.constructorName, "constructor", '')
         # Determining self.currentContent's location and attribute
-        if tag == "name":
-            if not (self.dictTag["expr"] or self.dictTag["expr_stmt"]):
+        if tag == "name" or tag == "index":
+            if not (self.dictTag["expr"] or self.dictTag["expr_stmt"] or (self.previousTag == "index" and tag == "name")):
                 if not (self.dictTag["type"]) or self.dictTag["parameter_list"]:
-                    if self.dictTag["class"] and not (self.dictTag["decl"] or self.dictTag["decl_stmt"] or self.dictTag["function_decl"] or self.dictTag["function"] or self.dictTag["parameter"] or self.dictTag["parameter_list"] or self.dictTag["constructor"]):
-                        print("Class Name: ", self.currentContent)
-                        self.className = self.currentContent
-                        self.XMLWriter("class",self.currentContent, '', self.xmlResult)
-                    elif self.dictTag["constructor"] and not (self.dictTag["decl"] or self.dictTag["decl_stmt"] or self.dictTag["parameter"]):
-                        print("Constructor Name: ", self.currentContent)  
-                        self.constructorName = self.currentContent
-                    elif (self.dictTag["interface"] or self.dictTag["interface_decl"]) and not(self.dictTag["function_decl"] or self.dictTag["function"] or self.dictTag["parameter"] or self.dictTag["parameter_list"]):
-                        print("Interface Name: ", self.currentContent)
-                        self.interfaceName = self.currentContent 
-                        self.XMLWriter("interface", self.currentContent, '', self.xmlResult)          
-                    elif self.dictTag["parameter_list"]:
-                        if self.dictTag["parameter"]and self.dictTag["type"]:
-                            print("Parameter Type: ", self.currentContent)
-                            self.parameterType.append(self.currentContent)
-                        else:
-                            print("Parameter Name: ", self.currentContent)
-                            self.parameterList.append(self.currentContent)
-                    elif (self.dictTag["decl_stmt"] or self.dictTag["decl"]) and not (self.dictTag["parameter"] or self.dictTag["parameter_list"]):
-                        print("Variable Name: ", self.currentContent)
-                        self.variableName = self.currentContent
-                        self.functionConstructorDeclXMLWriter(self.variableName, "decl", self.variableType) 
-                    elif (self.dictTag["function"] or self.dictTag["function_decl"]) and not (self.dictTag["parameter"] or self.dictTag["parameter_list"]):
-                        print("Function Name: ", self.currentContent)
-                        self.functionName = self.currentContent
+                    if self.currentContent.isidentifier() or "$" in self.currentContent or "[]" in self.currentContent or ">" in self.currentContent:
+                        if self.dictTag["class"] and not (self.dictTag["decl"] or self.dictTag["decl_stmt"] or self.dictTag["function_decl"] or self.dictTag["function"] or self.dictTag["parameter"] or self.dictTag["parameter_list"] or self.dictTag["constructor"]):
+                            print("Class Name: ", self.currentContent)
+                            self.className = self.currentContent
+                            self.XMLWriter("class",self.currentContent, '', self.xmlResult)
+                        elif self.dictTag["constructor"] and not (self.dictTag["decl"] or self.dictTag["decl_stmt"] or self.dictTag["parameter"]):
+                            print("Constructor Name: ", self.currentContent)  
+                            self.constructorName = self.currentContent
+                        elif (self.dictTag["interface"] or self.dictTag["interface_decl"]) and not(self.dictTag["function_decl"] or self.dictTag["function"] or self.dictTag["parameter"] or self.dictTag["parameter_list"]):
+                            print("Interface Name: ", self.currentContent)
+                            self.interfaceName = self.currentContent 
+                            self.XMLWriter("interface", self.currentContent, '', self.xmlResult)
+                        elif self.dictTag["parameter_list"]:
+                            if self.dictTag["parameter"]and self.dictTag["type"]:
+                                if(self.dictTag["index"]):
+                                    self.parameterTypeString = self.parameterTypeString+"[]"
+                                    self.parameterType.remove(self.parameterType[-1])
+                                    self.parameterType.append(self.parameterTypeString)
+                                if (self.dictTag["argument_list"]):
+                                    if(self.dictTag["argument"]):
+                                        if self.parameterTypeString[-1] == '>':
+                                            self.parameterTypeString= self.parameterTypeString[:-1]
+                                            self.parameterTypeString= self.parameterTypeString+","+self.currentContent+">"
+                                        else:
+                                            self.parameterTypeString = self.parameterTypeString+"<"+self.currentContent+">"
+                                            #asciiString = ''.join(str(ord(c)) for c in self.functionType)
+                                            #self.functionType = asciiString
+                                            self.parameterType.remove(self.parameterType[-1])
+                                            self.parameterType.append(self.parameterTypeString)
+                                else:
+                                    if self.currentContent != ">":
+                                        self.parameterTypeString = self.currentContent
+                                        print("Parameter Type: ", self.parameterTypeString)
+                                        self.parameterType.append(self.parameterTypeString)
+                            else:
+                                print("Parameter Name: ", self.currentContent)
+                                self.parameterList.append(self.currentContent)
+                        elif (self.dictTag["decl_stmt"] or self.dictTag["decl"]) and not (self.dictTag["parameter"] or self.dictTag["parameter_list"]):
+                            print("Variable Name: ", self.currentContent)
+                            self.variableName = self.currentContent
+                            self.functionConstructorDeclXMLWriter(self.variableName, "variable", self.variableType) 
+                        elif (self.dictTag["function"] or self.dictTag["function_decl"]) and not (self.dictTag["parameter"] or self.dictTag["parameter_list"]):
+                            print("Function Name: ", self.currentContent)
+                            self.functionName = self.currentContent
                 elif(self.dictTag["type"]):
                     if (self.dictTag["function"] or self.dictTag["function_decl"]) and not(self.dictTag["decl_stmt"] or self.dictTag["decl"]):
-                        print("Function Type: ", self.currentContent)
-                        self.functionType = self.currentContent
+                        if(self.dictTag["index"]):
+                            self.functionType = self.functionType+"[]"
+                        if (self.dictTag["argument_list"]):
+                            if(self.dictTag["argument"]):
+                                if self.functionType[-1] == '>':
+                                    self.functionType = self.functionType[:-1]
+                                    self.functionType = self.functionType+","+self.currentContent+">"
+                                else:
+                                    self.functionType = self.functionType+"<"+self.currentContent+">"
+                                    #asciiString = ''.join(str(ord(c)) for c in self.functionType)
+                                    #self.functionType = asciiString
+                        else:
+                            if self.currentContent != ">":
+                                self.functionType = self.currentContent
+                        print("Function Type: ", self.functionType)
                     else:
-                        print("Variable Type: ", self.currentContent)
-                        self.variableType = self.currentContent             
+                        if(self.dictTag["index"]):
+                            self.variableType= self.variableType+"[]"
+                        if (self.dictTag["argument_list"]):
+                            if(self.dictTag["argument"]):
+                                if self.variableType[-1] == '>':
+                                    self.variableType = self.variableType[:-1]
+                                    self.variableType = self.variableType+","+self.currentContent+">"
+                                else:
+                                    self.variableType = self.variableType+"<"+self.currentContent+">"
+                                    #asciiString = ''.join(str(ord(c)) for c in self.functionType)
+                                    #self.functionType = asciiString
+                        else:
+                            if self.currentContent != ">" and self.currentContent != "[]":
+                                self.variableType = self.currentContent
+                        print("Variable Type: ", self.variableType)
+                else:
+                    raise NameError(self.currentContent + " is not a valid identifier")
             self.dictTag[tag] -= 1
+            self.previousTag = tag
         #elif tag == "specifier":
             #print("Specifier: ", self.currentContent)
             #self.dictTag[tag] -= 1
@@ -163,6 +213,6 @@ if (__name__ == "__main__"):
     parser.setFeature(xml.sax.handler.feature_namespaces,0)  
     Handler = JavaHandler()  
     parser.setContentHandler(Handler)
-    parser.parse("die.xml")
+    parser.parse("2DArrayExample.xml")
     finalXMLResult = prettify(Handler.xmlResult) 
     print(finalXMLResult)
