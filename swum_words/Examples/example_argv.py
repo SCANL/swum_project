748916fbbe84319ea052e85f6f05ad0d2fa0e2dc
Aditya Bhargava <56522465+Bhargavaa1@users.noreply.github.com>
2020-08-07 11:30:46 -0400

    Delete example_argv.py

--- swum_words/Examples/example_argv.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def main(argv):
-    sys.argv[0]
-    for index, arg in enumerate(argv):
-        print('argument {}: {}'.format(index))
-
-if __name__ == '__main__':
-    main(sys.argv)
